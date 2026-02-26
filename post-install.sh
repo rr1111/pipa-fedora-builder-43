2c9c2737789834d5bd309b1eae47dc937f90730f
rr1111 <45209447+rr1111@users.noreply.github.com>
2026-02-26 23:25:11 +0100

    add post install script
    because i dont know if i can build images with the codecs installed and still have to figure out how to preinstall flatpaks

---
+++ post-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# add flathub remote and install bazaar, resources
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub io.github.kolunmi.Bazaar net.nokyan.Resources
+
+# install some common codecs and libs
+zenity --password | sudo -S dnf -y install ffmpeg-free aom libmpeg2 schroedinger
+
+#notify of completion
+zenity --notification --text="Done,
+installed codecs, added flathub remote"
