d692971ba80710a6ac4285bef945a3695ef8b65c
rr1111 <45209447+rr1111@users.noreply.github.com>
2026-02-26 23:28:34 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# install some common codecs and libs
+zenity --password | sudo -S dnf -y install ffmpeg-free aom libmpeg2 schroedinger
+
 # add flathub remote and install bazaar, resources
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub io.github.kolunmi.Bazaar net.nokyan.Resources
-
-# install some common codecs and libs
-zenity --password | sudo -S dnf -y install ffmpeg-free aom libmpeg2 schroedinger
 
 #notify of completion
 zenity --notification --text="Done,
