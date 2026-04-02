65184f9e79c9c70ae890d48bba84de546c0f3292
rr1111 <renerinner114@outlook.de>
2026-04-02 17:47:19 +0200

    fixes

--- build.sh
+++ build.sh
@@ -171,10 +171,6 @@ make_image() {
     arch-chroot $image_mnt systemctl enable qbootctl.service bootmac-bluetooth.service tuned.service tuned-ppd.service
     arch-chroot $image_mnt systemctl disable iio-sensor-proxy.service
     echo "### Enabling Desktop services"
-    case "$mkosi_profile" in
-        "" )   arch-chroot "$image_mnt" systemctl set-default multi-user.target ;;
-        * )    arch-chroot "$image_mnt" systemctl set-default graphical.target ;;
-    esac
     if [[ "$mkosi_profile" == "plasma" ]]; then
         arch-chroot $image_mnt systemctl enable plasmalogin.service
     elif [[ "$mkosi_profile" == "plasma-mobile" ]]; then
