72362293ab0b8b754939e5fb8f0c6f149f24872f
rr1111 <renerinner114@outlook.de>
2026-04-03 16:28:06 +0200

    attempt fixing niri

--- build.sh
+++ build.sh
@@ -202,7 +202,7 @@ make_image() {
         arch-chroot $image_mnt systemctl enable gdm.service
     elif [[ "$mkosi_profile" == "niri" ]]; then
         #dms greeter breaks mkosi its installed afterwards
-        arch-chroot $image_mnt dnf -y install dms-greeter
+        arch-chroot $image_mnt dnf -y install dms dms-greeter
         arch-chroot $image_mnt dms greeter enable
         arch-chroot $image_mnt dms greeter sync
     fi
